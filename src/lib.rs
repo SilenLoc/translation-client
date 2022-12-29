688ef8b707988b625ea22f19a376cfae8f991e0b
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-12-29 20:35:45 +0100

    chore: expose Ready enum

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod client;
 mod request;
 pub use crate::client::{
     examples::{examplenewtrans, exampletranslate},
-    maintenance::ready,
+    maintenance::{ready, Ready},
     newtrans::newtrans,
     translate::translate,
 };
